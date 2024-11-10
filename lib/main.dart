e044a0725d670aaa17c4093bd69a0da4a80fd86b
Pedro Alberto <edlavioe@gmail.com>
2024-11-10 11:51:29 +0100

    step-2

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:mynotes/views/homepage_view.dart';
+import 'package:mynotes/views/login_view.dart';
+import 'package:mynotes/views/register_view.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
   runApp(MaterialApp(
-    title: 'Flutter Demo',
-    theme: ThemeData(
-      primarySwatch: Colors.blue,
-    ),
-    home: const HomePage(),
+    title: 'My Notes',
+    theme: ThemeData(primarySwatch: Colors.blue, useMaterial3: true),
+    initialRoute: '/',
+    routes: {
+      '/': (context) => const HomePage(),
+      '/login': (context) => const LoginView(),
+      '/register': (context) => const RegisterView(),
+    },
   ));
 }
--- lib/views/homepage_view.dart
+++ lib/views/homepage_view.dart
@@ -2,6 +2,8 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:mynotes/firebase_options.dart';
+import 'package:mynotes/views/login_view.dart';
+import 'package:mynotes/views/verify_email_view.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -13,33 +15,24 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          'Home',
-          style: TextStyle(color: Colors.white),
-        ),
-        backgroundColor: Colors.blue,
+    return FutureBuilder(
+      future: Firebase.initializeApp(
+        options: DefaultFirebaseOptions.currentPlatform,
       ),
-      body: FutureBuilder(
-        future: Firebase.initializeApp(
-          options: DefaultFirebaseOptions.currentPlatform,
-        ),
-        builder: (context, snapshot) {
-          switch (snapshot.connectionState) {
-            case ConnectionState.done:
-              final user = FirebaseAuth.instance.currentUser;
-              if (user!.emailVerified) {
-                print("You're a verified user");
+      builder: (context, snapshot) {
+        switch (snapshot.connectionState) {
+          case ConnectionState.done:
+            final user = FirebaseAuth.instance.currentUser;
+            /* if (user!.emailVerified) {
+                return const Text('Welcome to MyNotes');
               } else {
-                print("You need verify your email first");
-              }
-              return const Text('Welcome to MyNotes');
-            default:
-              return const Text('Loading...');
-          }
-        },
-      ),
+                return const VerifyEmailView();
+              } */
+            return LoginView();
+          default:
+            return const CircularProgressIndicator();
+        }
+      },
     );
   }
 }
